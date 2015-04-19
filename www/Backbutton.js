6201afddfc44c6d75dea0f864e84f4457887aeff
unknown <dev.maleki@gmail.com>
2015-04-19 18:38:54 +0430

    I forgot to add method to www

--- www/Backbutton.js
+++ www/Backbutton.js
@@ -4,6 +4,10 @@ var Backbutton = {
         cordova.exec(successCallback, failureCallback, 'BackbuttonPlugin',
             'goHome', []);
     }
+    goHome: function(successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, 'BackbuttonPlugin',
+            'goBack', []);
+    }
 };
 
 module.exports = Backbutton;
